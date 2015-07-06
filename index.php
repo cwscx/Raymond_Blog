80f8a3d96eb31cc99b533d5b8fe9e9ea5b1fee45
Shengyang Shi <shengyang.shi@hotmail.com>
2015-07-06 22:11:20 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Raymond Shi's homepage</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+</head>
+<body>
+	<div class="container">
+		<!-- nav bar -->
+		<div id="menu" class="navbar navbar-default navbar-fixed-top">
+			<div class="navbar-header">
+				<button class="btn btn-info navbar-toggle">
+					<span class="glyphicon glyphicon-th-large"></span>
+				</button>
+				<div class="navbar-brand">
+					<a href="." style="text-decoration:none"><h3>Raymond's Blog</h3></a>
+				</div>
+			</div>
+
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active nav"><a href=".">Home</a></li>
+					<li class="nav"><a href="about.php">About</a></li>
+					<li class="nav"><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+		<h1>I'm still working on it</h1>
+	</div>
+</body>
+</html>

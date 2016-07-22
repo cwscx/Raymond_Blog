c001c0351d1f5a87f7abdf4e58f45a5acd5f9fda
Shengyang Shi <shengyang.shi@hotmail.com>
2016-07-22 00:08:23 -0700

    Modified web tag name

--- lib/template/category.tmpl.php
+++ lib/template/category.tmpl.php
@@ -2,7 +2,7 @@
 <?php
 	echo "<li><a href='.?search=category&val=Infrastructure' style='font-size:17px'>Infrastructure</a></li>";
 	echo "<li><a href='.?search=category&val=MachineLearning' style='font-size:17px'>Machine Learning</a></li>";
-	echo "<li><a href='.?search=category&val=WebDevelopment' style='font-size:17px'>Web Development</a></li>";
+	echo "<li><a href='.?search=category&val=Web' style='font-size:17px'>Web Development</a></li>";
 	echo "<li><a href='.?search=category&val=life' style='font-size:17px'>life</a></li>";
 ?>
 </ul>

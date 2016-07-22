b67aca561800e8075fceedfc990508e0ab2c2771
Shengyang Shi <shengyang.shi@hotmail.com>
2016-07-22 00:07:36 -0700

    navbar updated

--- lib/template/category.tmpl.php
+++ lib/template/category.tmpl.php
@@ -1,8 +1,8 @@
 <ul class="nav nav-tabs">
 <?php
-	echo "<li><a href='.?search=category&val=js' style='font-size:17px'>js</a></li>";
-	echo "<li><a href='.?search=category&val=php' style='font-size:17px'>php</a></li>";
-	echo "<li><a href='.?search=category&val=python' style='font-size:17px'>python</a></li>";
+	echo "<li><a href='.?search=category&val=Infrastructure' style='font-size:17px'>Infrastructure</a></li>";
+	echo "<li><a href='.?search=category&val=MachineLearning' style='font-size:17px'>Machine Learning</a></li>";
+	echo "<li><a href='.?search=category&val=WebDevelopment' style='font-size:17px'>Web Development</a></li>";
 	echo "<li><a href='.?search=category&val=life' style='font-size:17px'>life</a></li>";
 ?>
 </ul>
